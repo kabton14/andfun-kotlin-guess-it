f6ab947d429a672adbe1ce404c732df923821023
Kevin Beaton <kabton14@gmail.com>
2021-10-30 02:13:36 -0500

    Add view model for score fragment

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -22,6 +22,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
@@ -31,6 +33,10 @@ import com.example.android.guesstheword.databinding.ScoreFragmentBinding
  * Fragment where the final score is shown, after the game is over
  */
 class ScoreFragment : Fragment() {
+
+
+private lateinit var viewModel: ScoreViewModel
+private lateinit var viewModelFactory: ScoreViewModelFactory
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -46,10 +52,25 @@ class ScoreFragment : Fragment() {
                 false
         )
 
+        viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(arguments!!).score)
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(ScoreViewModel::class.java)
+
         // Get args using by navArgs property delegate
         val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
-        binding.scoreText.text = scoreFragmentArgs.score.toString()
-        binding.playAgainButton.setOnClickListener { onPlayAgain() }
+
+        binding.playAgainButton.setOnClickListener {
+            viewModel.playAgain()
+        }
+
+        viewModel.score.observe(this, Observer { finalScore ->
+            binding.scoreText.text = finalScore.toString()
+        })
+
+        viewModel.eventPlayAgain.observe(this, Observer { playAgain ->
+            if (playAgain) {
+                onPlayAgain()
+            }
+        })
 
         return binding.root
     }
